373ed18f3925b9fb59843d25376b5c1936818e44
dsalato <d.salato@mail.ru>
2023-10-17 20:49:06 +0700

    начало работы с redux toolkit, создание первого slice

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+import filterSlice from '../redux/slices/filterSlice'
+
+export const store = configureStore({
+    reducer: {
+        filter: filterSlice,
+
+    },
+})
